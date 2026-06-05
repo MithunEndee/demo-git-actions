ae3b8c3da40013dc716f79f9cae835f59f6c4514
MithunEndee <mithun@endee.io>
2026-06-05 11:29:10 +0530

    version update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     # Package Metadata
     name="endee",
-    version="0.1.27b1",
+    version="0.1.27",
     author="",
     author_email="",
     description=(
