a14584db7d10cbaa4f7190b0d0ded5eae2645c5a
MithunEndee <mithun@endee.io>
2026-08-07 17:47:23 +0530

    test repo

--- llama_index_endee/utils.py
+++
@@ -1,7 +0,0 @@
-"""Sparse encoding utilities for EndeeVectorStore.
-
-.. deprecated::
-    This module is deprecated. Use ``sparse_embeddings`` instead.
-    The ``SparseEmbeddings`` ABC, ``EndeeModelSparse``, and
-    ``SparseModelAdapter`` classes provide a cleaner interface.
-"""
